eeddd437ef66497db57214515c8e311e08bb5d20
Peter-240 <ngiriyabandijeanpierre@gmail.com>
2025-09-24 08:57:40 +0200

    Active save button

---
+++ pass.js
@@ -0,0 +1,25 @@
+
+let count = 0;
+
+function increament() {
+  
+  count = count + 1;
+
+  if (count === 10) {
+  
+    console.log('you win');
+  }
+  
+  document.querySelector(".count").innerHTML = ` ${count}`;
+  
+  console.log('button was clicked');
+ 
+  }
+function saveData() {
+  let saveButtonElement = document.querySelector('.save-btn')
+  let data = count + "-";
+
+  saveButtonElement.innerHTML += data;
+  }
+
+
